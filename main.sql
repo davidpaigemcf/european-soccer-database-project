8c4c55e79a891881aea236b516a538d96da0b023
David <118395567+davidpaigemcf@users.noreply.github.com>
2022-12-24 10:20:55 -0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -41,3 +41,17 @@ ORDER BY
     goals DESC
 	LIMIT 10;
 -- CONCLUSION: FC Barcelona scored the most goals- 112 goals- during 2015/2016
+
+-- How old were top 10 players for 2008-16?
+SELECT 
+	rank() over(order by ROUND(AVG(Player_Attributes.overall_rating), 1) desc) as Ranking,
+    Player.player_name AS Name,
+	FLOOR((julianday('2016-12-31')-julianday(Player.birthday))/365.25) AS Age,
+	ROUND(AVG(Player_Attributes.overall_rating), 1) AS Rating
+FROM Player 
+JOIN Player_Attributes  ON 
+    Player.player_api_id = Player_Attributes.player_api_id
+GROUP BY 
+    Player.player_api_id
+LIMIT 10;
+-- Conclusion: Lionel Messi had the highest average rating between 2008-16
